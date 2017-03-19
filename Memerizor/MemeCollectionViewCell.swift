e018ccf3ef8f6061ded752c62b17e9f2b9dc9f96
twokey <twokey@node-1w7jr9qpbvd6772nwen1eleo8.ipv6.telus.net>
2017-03-18 17:09:56 -0700

    Merge master to circular collection and bring all files from master for collection view

---
+++ Memerizor/MemeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MemeCollectionViewCell.swift
+//  Memerizor
+//
+//  Created by Kirill Kudymov on 2017-03-07.
+//  Copyright © 2017 Kirill Kudymov. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var memeCellImageView: UIImageView?
+    
+    
+}
